21f615e6586ab795a21870d0049b9a637fcd050b
lihui557 <lihui557@jd.com>
2021-04-19 22:10:15 +0800

    Temp: a little refactoring

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,22 +1,23 @@
-import { useRef } from 'react';
+import { FormEvent, useCallback, useRef } from 'react';
 import { addTodo } from '../actions';
 import { useAppDispatch } from '../hooks';
 
 export default () => {
     const inputRef = useRef<HTMLInputElement>(null);
     const dispatch = useAppDispatch();
+    const onSubmit = useCallback((e: FormEvent) => {
+        e.preventDefault();
+        if (!inputRef.current?.value.trim()) {
+            return;
+        }
+        if (inputRef.current) {
+            dispatch(addTodo(inputRef.current.value));
+            inputRef.current.value = '';
+        }
+    }, [dispatch]);
     return (
         <section>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                if (!inputRef.current?.value.trim()) {
-                    return;
-                }
-                if (inputRef.current) {
-                    dispatch(addTodo(inputRef.current.value));
-                    inputRef.current.value = '';
-                }
-            }}>
+            <form onSubmit={onSubmit}>
                 <input type="text" ref={inputRef}/>
                 <button type="submit">Add Todo</button>
             </form>
--- src/components/TodoFilters.tsx
+++ src/components/TodoFilters.tsx
@@ -3,23 +3,36 @@ import { setVisibilityFilter } from '../actions';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { VisibilityFilter } from '../types';
 
+const FilterButton = (props: {
+    filter: VisibilityFilter
+}) => {
+    const filterMap: {[key in VisibilityFilter]: string} = {
+    }
+    return (
+        <button ></button>
+    );
+}
+
 export default () => {
     const filter = useAppSelector(state => state.filter);
     const dispatch = useAppDispatch();
+    const onFilterClick = useCallback((filter: VisibilityFilter) => (e: MouseEvent) => {
+        dispatch(setVisibilityFilter(filter));
+    }, [dispatch]);
     return (
         <section>
             <button 
-                onClick={e => {dispatch(setVisibilityFilter(VisibilityFilter.SHOW_ALL))}} 
+                onClick={onFilterClick(VisibilityFilter.SHOW_ALL)} 
                 disabled={filter === VisibilityFilter.SHOW_ALL}>
                     SHOW ALL
             </button>
             <button 
-                onClick={e => {dispatch(setVisibilityFilter(VisibilityFilter.SHOW_COMPLETE))}} 
+                onClick={onFilterClick(VisibilityFilter.SHOW_COMPLETE)} 
                 disabled={filter === VisibilityFilter.SHOW_COMPLETE}>
                     SHOW COMPLETED
             </button>
             <button 
-                onClick={e => {dispatch(setVisibilityFilter(VisibilityFilter.SHOE_ACTIVE))}} 
+                onClick={onFilterClick(VisibilityFilter.SHOE_ACTIVE)} 
                 disabled={filter === VisibilityFilter.SHOE_ACTIVE}>
                     SHOW ACTIVE
             </button>
